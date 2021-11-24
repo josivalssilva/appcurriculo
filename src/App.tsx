f1300fba381d3f94bf7019d34a5ded266a4412c7
josivalssilva <josivalssilva@gmail.com>
2021-11-23 21:13:35 -0300

    primeiro commit, foto sendo apresentada

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyleSheet, View, Image, Text} from 'react-native';
+import {} from 'react-icons';
 
 import foto from './assets/foto.jpg';
 
@@ -9,14 +10,13 @@ const App = () => {
       <View style={style.page}>
         <View style={style.container_cabecalho}>
           <Image source={foto} style={style.foto} />
-          <Text>Josival dos Santos Silva</Text>
-          <Text>Desenvolvedor Web</Text>
-<View>
-  <Text>Facebook</Text>
-  <Text>Linkedin</Text>
-  <Text>GitHub</Text>
-</View>
-
+          <Text style={style.nome}>Josival dos Santos Silva</Text>
+          <Text style={style.funcao}>Desenvolvedor Web</Text>
+          <View>
+            <Text>Facebook</Text>
+            <Text>Linkedin</Text>
+            <Text>GitHub</Text>
+          </View>
         </View>
       </View>
     </>
@@ -25,7 +25,7 @@ const App = () => {
 
 const style = StyleSheet.create({
   page: {
-    backgroundColor: 'E7E7E7',
+    backgroundColor: '0000FF',
     flex: 1,
   },
   container_cabecalho: {
@@ -37,8 +37,16 @@ const style = StyleSheet.create({
     width: 200,
     height: 200,
     borderRadius: 125,
+    marginBottom: 10,
   },
-  nome: {}
+  nome: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  funcao: {
+    color: '#939341',
+    marginBottom: 15,
+  },
 });
 
 export default App;
